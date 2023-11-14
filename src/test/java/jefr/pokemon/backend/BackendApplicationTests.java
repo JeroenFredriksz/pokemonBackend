44d9f7669dbc03cf94990cf68ec87582a752a17b
JefrDev <Jeroen.fredriksz@gmail.com>
2023-11-14 13:11:34 +0100

    Initial commit

---
+++ src/test/java/jefr/pokemon/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jefr.pokemon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
